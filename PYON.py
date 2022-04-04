475db39aef7282d5aee276eb7c86933e7e99172e
Josh West <josh.west@centre.edu>
2022-04-04 10:04:32 -0400

    more set up

--- PYON.py
+++ PYON.py
@@ -1,8 +1,21 @@
 parsed = {}
-filename = "easy"
-with open(filename, "r") as file:
-    data = file.read()
-parse_s(s):
+filename = "easy_test.json"
+
+def parse_s(s):
+    print("parse_s called") #works
     if (s[0] == '{'):
         #add the literal value to dictionary
+        pass
+    else:
+        reason = "some specific reason"
+        print("Error! {reason}")
 
+def main():
+    print("Hello World!")
+    with open(filename, "r") as file:
+        data = file.read()
+    parse_s(data)
+
+
+if __name__ == "__main__":
+    main()
